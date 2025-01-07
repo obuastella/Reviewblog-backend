bc0c3ab0ac992885973cff0cb65573007f6c5ec3
Obua Stella <stellaobua123@gmail.com>
2025-01-07 19:31:48 +0100

    tested with postman

--- server.ts
+++ server.ts
@@ -1,9 +1,18 @@
 import dotenv from "dotenv";
 import { dbConnect } from "./config/dbconnect";
 const express = require("express");
+import { Request, Response } from "express";
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
+//health check
+app.get("/api/", (req: Request, res: Response) => {
+  res.status(200).send("ReviewBlog Backend!");
+});
+
+// start server
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
 dbConnect();
