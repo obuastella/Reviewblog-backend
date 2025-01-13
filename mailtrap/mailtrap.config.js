d83bb5258581225e79f34da26af0ea508ebb1aee
Obua Stella <stellaobua123@gmail.com>
2025-01-13 22:49:04 +0100

    fixed mailtrap issue

---
+++ mailtrap/mailtrap.config.js
@@ -0,0 +1,27 @@
+import { MailtrapClient } from "mailtrap";
+
+const TOKEN = "a2de42414581273e2bf3187fe9690c91";
+
+const client = new MailtrapClient({
+  token: TOKEN,
+});
+
+const sender = {
+  email: "hello@demomailtrap.com",
+  name: "Mailtrap Test",
+};
+const recipients = [
+  {
+    email: "stellaobua123@gmail.com",
+  },
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
